5f7b44ca26abafad308bb3109961f500432ce879
eojin0814 <leeeojin6@gmail.com>
2024-03-11 22:59:35 +0900

    feat/init

---
+++ src/test/java/com/eureka/concertservice/ConcertserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.concertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
